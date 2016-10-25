6c06643ecccd3d1c3838bf2b0555efef8c78d221
Ben Webb <ben@salilab.org>
2016-10-25 12:24:35 -0700

    Add basic tests of the results page.

---
+++ test/frontend/test_results.pl
@@ -0,0 +1,63 @@
+use saliweb::Test;
+use Test::More 'no_plan';
+use Test::Exception;
+use File::Temp qw(tempdir);
+
+BEGIN {
+    use_ok('cryptosite');
+}
+
+my $t = new saliweb::Test('cryptosite');
+
+# Check results page
+
+# Check display_ok_job
+{
+    my $frontend = $t->make_frontend();
+    my $job = new saliweb::frontend::CompletedJob($frontend,
+                        {name=>'testjob', passwd=>'foo', directory=>'/foo/bar',
+                         archive_time=>'2009-01-01 08:45:00'});
+    my $ret = $frontend->display_ok_job($frontend->{CGI}, $job); 
+    like($ret, '/Job.*testjob.*has completed.*chimera\.zip.*' .
+               'UCSF Chimera session file/ms', 'display_ok_job');
+}
+
+# Check display_failed_job
+{
+    my $frontend = $t->make_frontend();
+    my $job = new saliweb::frontend::CompletedJob($frontend,
+                        {name=>'testjob', passwd=>'foo', directory=>'/foo/bar',
+                         archive_time=>'2009-01-01 08:45:00'});
+    my $ret = $frontend->display_failed_job($frontend->{CGI}, $job); 
+    like($ret, '/Your CryptoSite job.*testjob.*failed to produce any ' .
+               'prediction.*' .
+               'please see the.*#errors.*help page.*For more information, ' .
+               'you can.*framework\.log.*download the CryptoSite file\-check ' .
+               'log file.*' .
+               'contact us/ms',
+         'display_failed_job');
+}
+
+# Check get_results_page
+{
+    my $frontend = $t->make_frontend();
+    my $job = new saliweb::frontend::CompletedJob($frontend,
+                        {name=>'testjob', passwd=>'foo', directory=>'/foo/bar',
+                         archive_time=>'2009-01-01 08:45:00'});
+    my $tmpdir = tempdir(CLEANUP=>1);
+    ok(chdir($tmpdir), "chdir into tempdir");
+
+    my $ret = $frontend->get_results_page($job);
+    like($ret,
+         '/Your CryptoSite job.*testjob.*failed to produce any prediction/',
+         'get_results_page (failed job)');
+
+    ok(open(FH, "> cryptosite.pol.pred.pdb"), "Open cryptosite.pol.pred.pdb");
+    ok(close(FH), "Close cryptosite.pol.pred.pdb");
+
+    $ret = $frontend->get_results_page($job);
+    like($ret, '/Job.*testjob.*has completed/',
+         '                 (successful job)');
+
+    chdir("/");
+}
