dab99f6f854409ca6b2485bbd0ae6c22cc9f3a52
Ben Webb <ben@salilab.org>
2016-11-10 13:10:53 -0800

    Update to match latest CryptoSite library.

--- python/cryptosite/__init__.py
+++ python/cryptosite/__init__.py
@@ -223,7 +223,7 @@ date
 
             ### - gather the AM data
             self.logger.info("Gathering AllosMod results")
-            subprocess.check_call("module load cryptosite && cryptosite gatherer /scrapp/AM/%s %s" % (rfil,chainid))
+            subprocess.check_call("module load cryptosite && cryptosite gather /scrapp/AM/%s" % rfil)
 
             ### - run SVM
             ## TO DELETE
